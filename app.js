4dcbcedda05d19a9fc1b18ed74f39a2e4cbceacd
LeuxDeux <yoyoyzacarias@gmail.com>
2024-03-28 05:35:27 -0300

    primer commit get/

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('HOLA MUNDO');
+});
+
 app.listen(3000, (req, res)=>{
     console.log('SERVER RUNNING AT PORT 3000');
 });
