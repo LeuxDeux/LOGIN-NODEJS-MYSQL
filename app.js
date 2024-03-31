a4d1100f2ac7aa962003368d6478337f89156946
LeuxDeux <yoyoyzacarias@gmail.com>
2024-03-30 22:41:12 -0300

    frotnend básico y metodo de registro

--- app.js
+++ app.js
@@ -27,8 +27,42 @@ const connection = require('./database/db');
 //Rutas 
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', {msg: 'Lautaro'});
+    res.render('login', {msg: 'Lautaro'});
 });
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+//register
+
+app.post('/register', async (req, res)=>{
+    const user = req.body.user;
+    const name = req.body.name;
+    const rol = req.body.rol;
+    const pass = req.body.pass;
+    let passwordHaash = await bcryptjs.hash(pass,8);
+    connection.query('INSERT INTO users SET ?', {user:user, name:name, rol:rol, pass:passwordHaash}, async(error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.render('register', {
+                alert: true,
+                alertTitle: "Registro",
+                alertMessage: "¡Registro Exitoso!",
+                alertIcon: 'succes',
+                showConfirmButton: false,
+                timer: 1500,
+                ruta: ''
+            });
+        }
+    });
+});
+
+//autentificacion
 
 app.listen(3000, (req, res)=>{
     console.log('SERVER RUNNING AT PORT 3000');
